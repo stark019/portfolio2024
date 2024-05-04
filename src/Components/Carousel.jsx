e6567c73a03cad50a624be79b829f9a74085251e
stark019 <meettonihal@gmail.com>
2024-05-05 04:51:03 +0530

    fixed slick

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,27 +1,27 @@
-import React, { useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+// import React, { useRef } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-const Carousel = ({ children }) => {
-  const containerRef = useRef(null);
+// const Carousel = ({ children }) => {
+//   const containerRef = useRef(null);
 
-  const scroll = (scrollOffset) => {
-    containerRef.current.scrollLeft += scrollOffset;
-  };
+//   const scroll = (scrollOffset) => {
+//     containerRef.current.scrollLeft += scrollOffset;
+//   };
 
-  return (
-    <div className="carousel-container">
-      <div className="carousel-wrapper" ref={containerRef}>
-        {children}
-      </div>
-      <button className="carousel-arrow left" onClick={() => scroll(-300)}>
-        <FontAwesomeIcon icon={faArrowLeft} />
-      </button>
-      <button className="carousel-arrow right" onClick={() => scroll(300)}>
-        <FontAwesomeIcon icon={faArrowRight} />
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="carousel-container">
+//       <div className="carousel-wrapper" ref={containerRef}>
+//         {children}
+//       </div>
+//       <button className="carousel-arrow left" onClick={() => scroll(-300)}>
+//         <FontAwesomeIcon icon={faArrowLeft} />
+//       </button>
+//       <button className="carousel-arrow right" onClick={() => scroll(300)}>
+//         <FontAwesomeIcon icon={faArrowRight} />
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Carousel;
+// export default Carousel;
